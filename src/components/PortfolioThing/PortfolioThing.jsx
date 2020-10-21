474d679c9a4bccf541d25e2f47ded654990a0438
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-10-21 15:21:21 -0600

    assdf

--- src/components/PortfolioThing/PortfolioThing.jsx
+++ src/components/PortfolioThing/PortfolioThing.jsx
@@ -9,8 +9,14 @@ const PortfolioThing = props => {
         <div className="PortfolioThing" id={props.site.id}>
             <div className="info">
                 <h2>{props.site.title}</h2>
-                <p>{props.site.about} <br/><a target="_blank" href={props.site.url}>Visit Site
-                </a></p>
+                <p>
+                    <a target="_blank" href={props.site.url}>Visit Site</a> <br/>
+                    {props.site.about} <br/>
+                    <h3>technologies used: </h3>
+                    {props.site.technologies.map((tech, i)=> 
+                    <span>{tech}{i < props.site.technologies.length - 1 ? "," : "" } </span>
+                    )} 
+                </p>
                
             </div>
         </div>
