9fbc4d192b75d97566327eea98138c4dc873637b
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-10-21 15:37:37 -0600

    info

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,8 @@ const Header = props => {
         <div className="Header">
             <div className="profile-pic"></div>
             <h1>Charles MacMillan</h1>
-            <p>Dillon, Colorado</p>
+            <p>Full-Stack Web Developer <br/>
+            Dillon, Colorado</p>
             <div>
                 <a href="mailto:charlesmacmillanpro@gmail.com">Email</a> &nbsp;&nbsp;|&nbsp;&nbsp;
                 <a targer="_blank" href="https://github.com/charlesmacmillan">gitHub</a>&nbsp;&nbsp;|&nbsp;&nbsp;
